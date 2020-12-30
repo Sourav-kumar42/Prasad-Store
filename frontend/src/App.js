d4928676ecf577d3dd46bb1271c86bacd07c50ca
Sourav Kumar <sourav7244@gmail.com>
2020-12-30 21:18:49 +0530

    Added Home Screen Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreens from './screens/HomeScreen'
 const App = () => {
   return (
     <>
     <Header />
     <main className ="py-3">
     <Container>
-    <h1>Welcome to Prasad Store</h1>
+       <HomeScreens />
     </Container>
      
     </main>
