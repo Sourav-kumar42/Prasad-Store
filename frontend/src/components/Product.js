4b3e651c074d61e06058940b83662486cb6b87c2
Sourav Kumar <sourav7244@gmail.com>
2020-12-31 11:45:19 +0530

    Added rating react componenet

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,22 +1,19 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
-import products from '../products'
-
+import Rating from './Rating'
 const Product = ({product}) => {
     return (
         <Card className = "my-3 p-3 rounded">
-           <a href={`/product/${product._id}`}></a> 
+           <a href={`/products/${product._id}`}></a> 
            <Card.Img src={product.image} variant = 'top' />
            <Card.Body>
-           <a href={`/product/${product._id}`}> 
+           <a href={`/products/${product._id}`}> 
            <Card.Title as="div">
                <strong>{product.name}</strong>
            </Card.Title>
            </a>
            <Card.Text>
-               <div className="my-3">
-                  {product.rating} from {product.numReviews} reviews
-               </div>
+              <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
            </Card.Text>
            <Card.Text as="h3">${product.price}</Card.Text>
            </Card.Body>
